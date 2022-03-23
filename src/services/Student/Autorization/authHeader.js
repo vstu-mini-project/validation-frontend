40b6bbb9c0a8292cb324bccfa915a460a7e928c6
nikskul <nikitaskul2012@gmail.com>
2022-03-23 23:30:46 +0300

    reset

--- src/services/Student/Autorization/authHeader.js
+++ src/services/Student/Autorization/authHeader.js
@@ -1,6 +1,6 @@
 export default function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.accessToken) {
+    if (user && user.token) {
       return { Authorization: 'Bearer_ ' + user.token };
     } else {
       return {};

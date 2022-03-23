24f8817f1ea963573ac4cd6fe52c8aa641d9882e
kstyalitvinova039 <kstya.litvinova@gmail.com>
2022-03-23 19:11:44 +0300

    translate3.0

--- src/components/Authorization/AuthorizationComponent.js
+++ src/components/Authorization/AuthorizationComponent.js
@@ -31,9 +31,37 @@ const theme = createTheme();
 export default function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // TODO Добавить отправку формы
-  };
+    this.setState({
+      message: "",
+      loading: true
+    });
+    this.form.validateAll();
+    if (this.checkBtn.context._errors.length === 0) {
+      AuthService.login(this.state.username, this.state.password).then(
+        () => {
+          this.props.history.push("/profile");
+          window.location.reload();
+        },
+        error => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+          this.setState({
+            loading: false,
+            message: resMessage
+          });
+        }
+      );
+    } else {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+  
 
   return (
     <ThemeProvider theme={theme}>

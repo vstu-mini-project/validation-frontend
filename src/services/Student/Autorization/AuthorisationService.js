237d86c38124777e0baa0b90b0ac97f578fd7c97
kstyalitvinova039 <kstya.litvinova@gmail.com>
2022-03-23 19:08:13 +0300

    translate2.0

---
+++ src/services/Student/Autorization/AuthorisationService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+const API_URL = "http://localhost:8080/api/vi/";
+class AuthService {
+  login(username, password) {
+    return axios
+      .post(API_URL + "auth/login", {
+        username,
+        password
+      })
+      .then(response => {
+        if (response.data.token) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+      });
+  }
+  logout() {
+    localStorage.removeItem("user");
+  }
+  register(username, email, password) {
+    return axios.post(API_URL + "users", {
+      username,
+      email,
+      password
+    });
+  }
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));;
+  }
+}
+export default new AuthService();
---
+++ src/services/Student/Autorization/UserService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+const API_URL = 'http://localhost:8080/api/v1/';
+class UserService {
+  
+  getUserBoard() {
+    return axios.get(API_URL + 'users', { headers: authHeader() });
+  }
+  
+}
+export default new UserService();
